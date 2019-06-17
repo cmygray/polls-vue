eeedcff00ada56504af7c0aa09af88bec7e90882
cmygray <cmygray@gmail.com>
2019-06-17 18:15:46 +0900

    feature(store): 투표 목록을 최근 생성 순으로 정렬
    created_at 필드를 기준으로 상태의 값을 정렬하여 반환
    
    Signed-off-by: cmygray <cmygray@gmail.com>

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,4 +1,10 @@
 import { GetterTree } from 'vuex';
 import { State } from '@/store/state';
 
-export const getters: GetterTree<State, State> = {};
+export const getters: GetterTree<State, State> = {
+  allPolls: state =>
+    Object.values(state.polls).sort(
+      (currentPoll, nextPoll) =>
+        +new Date(nextPoll.created_at) - +new Date(currentPoll.created_at)
+    ),
+};
---
+++ tests/unit/store/getters.spec.ts
@@ -0,0 +1,44 @@
+import { getters } from '@/store/getters';
+import { State } from '@/store/state';
+import { poll } from '../__fixtures__';
+
+describe('getters', () => {
+  let state: State
+
+  beforeEach(() => {
+    state = { polls: {} }
+  })
+
+  describe('allPolls', () => {
+    beforeEach(() => {
+      state = {
+        polls: {
+          'older' : {
+            ...poll,
+            id: 'older',
+            created_at: '2019-06-13T17:47:36.840Z'
+          },
+          'now': {
+            ...poll,
+            id: 'now',
+            created_at: '2019-06-15T17:47:36.840Z'
+          },
+          'newer': {
+            ...poll,
+            id: 'newer',
+            created_at: '2019-06-17T17:47:36.840Z'
+          }
+        }
+      }
+    })
+
+    it('should return all polls as an array sorted by created at DESC', () => {
+      // @ts-ignore
+      expect(getters.allPolls(state)).toEqual([
+        state.polls.newer,
+        state.polls.now,
+        state.polls.older,
+      ])
+    })
+  })
+})
