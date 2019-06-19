4722f671b4d906fbf19e5636dd5220123b622a96
cmygray <cmygray@gmail.com>
2019-06-19 20:57:41 +0900

    fix(filter): UTC hour 대신 로컬 타임존 hour 사용
    yyyy-MM-dd hh:mm 포맷팅 시 UTCHours를 사용하여 시간 표시에 오차가 생기는 버그 수정
    
    Signed-off-by: cmygray <cmygray@gmail.com>

--- src/filters/format-timestamp.ts
+++ src/filters/format-timestamp.ts
@@ -4,7 +4,7 @@ export function formatTimestamp(timestamp: string): string {
   const yyyy = datetime.getFullYear();
   const mm = String(datetime.getMonth() + 1).padStart(2, '0');
   const dd = String(datetime.getDate()).padStart(2, '0');
-  const HH = String(datetime.getUTCHours()).padStart(2, '0');
+  const HH = String(datetime.getHours()).padStart(2, '0');
   const MM = String(datetime.getMinutes()).padStart(2, '0');
 
   return `${yyyy}-${mm}-${dd} ${HH}:${MM}`;
--- tests/unit/filters/format-timestamp.spec.ts
+++ tests/unit/filters/format-timestamp.spec.ts
@@ -1,7 +1,7 @@
 import { formatTimestamp } from '@/filters/format-timestamp';
 
 describe('formatTimestamp', () => {
-  const TIME_STAMP = '2019-06-18T11:00:00.000Z'
+  const TIME_STAMP = '2019-06-18T11:00:00'
 
   it('should return "yyyy-mm-dd HH:MM" string', () => {
     expect(formatTimestamp(TIME_STAMP)).toEqual('2019-06-18 11:00')
