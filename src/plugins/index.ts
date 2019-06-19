d34e585100f34e236c030a7bd8a58e65ee43e170
cmygray <cmygray@gmail.com>
2019-06-19 20:40:23 +0900

    build(datetime): 날짜/시간 선택 라이브러리 의존성 추가
    투표의 시작,종료 시간 선택을 위해 vue-datetime 컴포넌트를 전역에 등록
    
    Signed-off-by: cmygray <cmygray@gmail.com>

---
+++ src/plugins/index.ts
@@ -0,0 +1,2 @@
+import './vue-datetime';
+import './vuetify';
---
+++ src/plugins/vue-datetime.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+// @ts-ignore
+import Datetime from 'vue-datetime';
+import 'vue-datetime/dist/vue-datetime.css';
+
+Vue.use(Datetime);
